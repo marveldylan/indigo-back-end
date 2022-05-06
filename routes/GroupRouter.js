d0f1b4f7f3b9f4085ca6d4a52004a71b2c70f81a
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-05-06 14:53:08 -0400

    added Group routes

--- routes/GroupRouter.js
+++ routes/GroupRouter.js
@@ -1,9 +1,38 @@
 const { Router } = require('express');
+const router = Router()
 const controller = require('../controllers/GroupController')
 const middleware = require('../middleware')
 
 //Routes
+//Routes
+router.get(
+    '/',
+    controller.getGroups
+)
 
+router.get(
+    '/:group_id',
+    controller.getGroupById
+)
+
+router.post(
+    './:group_id',
+    controller.createGroup
+)
+
+router.put(
+    '/:group_id',
+    controller.updateGroup
+)
+
+router.delete(
+    '/:group_id',
+    controller.deleteGroup
+)
+
+module.exports = (
+    router
+)
 module.exports = (
     Router
 )
