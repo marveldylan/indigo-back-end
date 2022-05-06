67ecdac5cf49665b73d5b2a11517ed45b54fe108
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-05-06 12:20:11 -0400

    created initial dev database w db/index.js

---
+++ db/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/indigo_database_development'
+
+mongoose
+    .connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => {
+        console.log('Successfully connected to MongoDB.')
+    })
+    .catch(e => {
+        console.error('Connection error', e.message)
+    })
+
+const db = mongoose.connection
+
+module.exports = db
