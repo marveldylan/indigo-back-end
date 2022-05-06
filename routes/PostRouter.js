63c0060529e8c945c1efcc24ea5f061a666d991c
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-05-06 14:51:20 -0400

    added post routes

--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -1,8 +1,38 @@
 const { Router } = require('express');
+const router = Router()
 const controller = require('../controllers/PostController')
 const middleware = require('../middleware')
 
 //Routes
+//Routes
+router.get(
+    '/',
+    controller.getPosts
+)
+
+router.get(
+    '/:post_id',
+    controller.getPostById
+)
+
+router.post(
+    './:post_id',
+    controller.createPost
+)
+
+router.put(
+    '/:post_id',
+    controller.updatePost
+)
+
+router.delete(
+    '/:post_id',
+    controller.deletePost
+)
+
+module.exports = (
+    router
+)
 
 module.exports = (
     Router
