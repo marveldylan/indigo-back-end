eee204155d372484dd9fd53d6a8575d62d81fa07
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-05-11 18:27:50 -0400

    same as last

--- models/category.js
+++ models/category.js
@@ -6,7 +6,6 @@ const Category = new Schema (
         name: { type: String, required: true },
         cover_image: { type: String, required: true },
         group_counter: { type: Number, required: true },
-        channel_counter: { type: Number, required: true },
         follower_counter: { type: Number, required: true }
     },
     { timestamps: true },
