a7d8a991f3f698d57b255523f806c06b06285efd
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-05-11 11:37:22 -0400

    added category collection - created category controller just for read

---
+++ controllers/CategoryController.js
@@ -0,0 +1,31 @@
+const { default: mongoose } = require('mongoose')
+const { Category } = require('../models')
+
+const getCategories = async (req, res) => {
+    try {
+        const categories = await Category.find()
+        return res.status(200).json({ category })
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const getCategoryById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const category = await Category.findById(id)
+        if (category) {
+            return res.status(200).json({ category });
+        }
+        return res.status(404).send('category with the specified ID does not exists');
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+
+
+module.exports = {
+    getCategories,
+    getCategoryById,
+}
