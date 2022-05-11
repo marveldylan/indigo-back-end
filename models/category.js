25016e2454c80d0d070b341c6618ae05174e3500
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-05-11 18:24:48 -0400

    added group_counter to category model

--- models/category.js
+++ models/category.js
@@ -5,6 +5,7 @@ const Category = new Schema (
     {
         name: { type: String, required: true },
         cover_image: { type: String, required: true },
+        group_counter: { type: Number, required: true },
         channel_counter: { type: Number, required: true },
         follower_counter: { type: Number, required: true }
     },
