1adabcbcc6b3e15538556a6dd14dbb2db4aa2f63
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-05-11 20:44:54 -0400

    update

--- controllers/GroupController.js
+++ controllers/GroupController.js
@@ -47,14 +47,14 @@ const getGroupById = async (req, res) => {
 
 const getGroupsByCategory = async (req, res) => {
     try {
-        let channelId = req.params.channel_id;
-        const group = await Group.findById({
-            category_id: channelId
+        let categoryId = req.params.category_id;
+        const groups = await Group.find({
+            category_id: categoryId
         })
-        if (group) {
-            return res.status(200).json({ group });
+        if (groups) {
+            return res.status(200).json({ groups });
         }
-        return res.status(404).send('Group with the specified ID does not exists');
+        return res.status(404).send('Groups with the specified Category ID does not exists');
     } catch (error) {
         return res.status(500).send(error.message);
     }
