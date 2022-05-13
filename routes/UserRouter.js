0b01f0f698aead7d729279a58bee0fcedbbc0d95
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-05-13 09:18:03 -0400

    added full user routes for following, unfollowing and disbanding groups and channels and saving posts

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -27,6 +27,49 @@ Router.put(
     controller.userUnfollowCategory
 )
 
+Router.put(
+    '/:id/unfollow/group/:group_id',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.userUnfollowGroup
+)
+
+Router.put(
+    '/:id/disband/group/:group_id',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.userDisbandGroup
+)
+
+Router.put(
+    '/:id/unfollow/channel/:channel_id',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.userUnfollowChannel
+)
+
+Router.put(
+    '/:id/disband/channel/:channel_id',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.userDisbandChannel
+)
+
+Router.put(
+    '/:id/disband/user/:user_id',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.userDisbandUser
+)
+
+Router.put(
+    '/:id/unfollow/user/:user_id',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.userUnfollowUser
+)
+
+
 module.exports = (
     Router
 )
